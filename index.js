b3c68af13258a3e1f448d6fedb0279f7e48d3324
Jacob Hooser <jacob.hooser@yahoo.com>
2022-02-22 15:17:26 -0600

    commit tree 0625fcb1a1db90aa968b934dc9ab415374471078

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    const hq = 42;
+    let distance = 0;
+    if(location > hq){
+        distance = location - hq;
+        return distance;
+    }
+    else{
+        distance = hq - location;
+        return distance;
+    }
+}
+function distanceFromHqInFeet(location){
+    const hq = 42;
+    let distance = 0;
+    if(location > hq){
+        distance = location - hq;
+        return distance*264;
+    }
+    else{
+        distance = hq - location;
+        return distance*264;
+    }
+}
+function distanceTravelledInFeet(start, destination){
+    let distance = 0;
+    if(start > destination){
+        distance = start - destination;
+        return distance*264;
+    }
+    else{
+        distance = destination - start;
+        return distance*264;
+    }
+}
+function calculatesFarePrice(start, destination){
+    let distance = 0;
+    if(start > destination){
+        distance = start - destination;
+        distance = distance * 264;
+    }
+    else{
+        distance = destination - start;
+        distance = distance * 264;
+    }
+    if (distance < 400){
+        return 0;
+    }
+    else if (distance >= 400 && distance <= 2000){
+        return (distance - 400) * 0.02;
+    }
+    else if (distance > 2000 && distance <= 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+}
